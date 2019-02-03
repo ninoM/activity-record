c01e506f66662a5f01285650c7f2e2f5fbf3e8c5
Niño Mollaneda <ninoo.mollaneda@gmail.com>
2019-02-04 02:15:13 +0800

    Add fitness as category

--- src/constant.js
+++ src/constant.js
@@ -3,7 +3,8 @@ import { Firebase } from './ApiLayer';
 export const ACTIVITY_CATEGORIES = {
   personal: "Personal",
   business: "Business",
-  leisure: "Rest and restoration"
+  leisure: "Rest and restoration",
+  fitness: "Fitness",
 }
 
 export const DEFAULT_ACTIVITY = {
